efb53de7c34be174a6e6055a9f96bb64d7751e2b
xconstruct <me@xconstruct.net>
2011-04-05 23:06:07 +0200

    erste uebung komplett

---
+++ 01/Fact.java
@@ -0,0 +1,23 @@
+/**
+ * @author Constantin Schomburg <me@xconstruct.net>
+ *
+ * Übung 01 - Programmieren (Java) - Gruppe 6
+ * 05.04.11 - 12.04.11
+ *
+ * Berechnet die Fakultät einer fest genannten Zahl.
+ */
+class Fact {
+
+	public static void main(String... args) {
+		int n = 5;
+
+		int i = n;
+		int factorial = 1;
+		while (i > 1) {
+			factorial *= i;
+			i--;
+		}
+
+		System.out.println("Factorial of " + n + " is " + factorial);
+	}
+}
